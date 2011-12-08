5e1f586702a0ae68ead2d8e75040307079f5d10f
luise5554 <luise5554@gmail.com>
2011-12-08 13:46:31 -0500

    ya podemos comenzar a modificar!!!

--- Mootify/src/co/edu/udea/campusmovil/mobile/MootifyActivity.java
+++ Mootify/src/co/edu/udea/campusmovil/mobile/MootifyActivity.java
@@ -2,7 +2,7 @@ package co.edu.udea.campusmovil.mobile;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+//Ahora pueden comenzar a modificar
 public class MootifyActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
