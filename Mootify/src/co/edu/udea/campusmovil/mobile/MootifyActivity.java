eddca316368b13ad8a71abb51c87394d0e8aff31
luise5554 <luise5554@gmail.com>
2011-12-08 13:36:38 -0500

    archivos a ignorar

---
+++ Mootify/src/co/edu/udea/campusmovil/mobile/MootifyActivity.java
@@ -0,0 +1,13 @@
+package co.edu.udea.campusmovil.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MootifyActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
